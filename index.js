fae5a4770a4026f532a8bff4d59bea357b801158
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-09-20 10:38:47 -0300

    Setting POST, PUT and DELETE

--- index.js
+++ index.js
@@ -35,3 +35,53 @@ app.route('/users/:id').get((req, res) => {
 
     res.send(user)
 })
+
+app.route('/user').post((req, res) => {
+const lastId = users[users.length -1].id
+
+    users.push({
+        id: lastId+1,
+        name: req.body.name,
+        last: req.body.last,
+        city: req.body.city,
+        bio: req.body.bio,
+        avatar: req.body.avatar
+    })
+    res.send('New User Save')
+})
+
+app.route('/user').put((req, res) => {
+    const idUser = req.params.id
+
+    const user = users.find(user => Number(user.id) === Number(idUser))
+
+    if (!user) {
+        res.send('Error 404 not found or user doest exist')
+    }
+
+    const updatedUser = {
+        ...user,
+        name: req.body.name,
+        last: req.body.last,
+        city: req.body.city,
+        bio: req.body.bio,
+        avatar: req.body.avatar
+    }
+
+    users = users.map(user => {
+        if (Number(user.id) === Number(userId)) {
+          user = updatedUser
+        }
+        return user
+    })
+
+    res.send('User updated')
+})
+
+app.route('/users/:id').delete((req, res) => {
+    const userId = req.params.id
+  
+    users = users.filter(user => Number(user.id) !== Number(userId))
+  
+    res.json('Deleted User')
+  })
